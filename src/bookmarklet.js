0c47d7c6eda215aa7db6bc09406375e45f8ccd9e
Nikhi <nikhibhambra@gmail.com>
2020-12-29 14:27:15 -0500

    think im almost there, need to figure out how to inject a whole minified bundled library but then itll work

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,13 +1,12 @@
-import "./html2pdf.bundle.js"
-
 if (window.location.href == 'https://acorn.utoronto.ca/sws/#/history/academic') {
     v = document.getElementsByClassName('hidden-print');
     if (v.length > 0) {
+        // Need to import or otherwise run html2pdf
         n = document.getElementById('acorn-nav-top');
         m = document.getElementById('acorn-nav-side-parent');
-        d = document.getElementsByClassName('academic-history');
-        f = document.getElementsByClassName("academic-history-report");
-        bt = document.getElementById('acorn-footer');
+        d = document.getElementsByClassName('academic-history'); 
+        f = document.getElementsByClassName('academic-history-report');
+        bt = document.getElementById('acorn-footer'); 
         n.remove(); m.remove(); bt.remove();
         t = document.getElementsByTagName('h1');
         name = v[1].innerHTML.match(/<strong[^>]*>([\s\S]*?)<\/strong>/)[1];
@@ -21,11 +20,11 @@ if (window.location.href == 'https://acorn.utoronto.ca/sws/#/history/academic') 
         x.style.justifyContent = 'center';
         k = document.getElementsByClassName('credit-earned-section');
         for (i = 0; i < k.length; i++) {
-            k[i].style.marginBottom = "15px";
+            k[i].style.marginBottom = '15px';
         }
-        bd = document.body;
-        html2pdf(bd);
-        alert('Go check your downloads! :)');
+        // bd = document.body;
+        // html2pdf(bd);
+        // alert('Go check your downloads! :)');
     } else {
         alert('See where it says Complete Academic History next to your name? Click that and try again.');
     }
