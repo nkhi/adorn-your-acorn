aae255f217c85c8b75b4f9b135bb8752bf21ca8f
Nikhi <nikhibhambra@gmail.com>
2020-12-30 01:26:57 -0500

    script working, need to tweak semester block styles tmo but looking good. need to exec html2pdf.bundle.min.js at end of bookmarklet.js

--- src/grades.php
+++
@@ -1,5 +0,0 @@
-// get document.body
-// scrape person's name, other info from here
-// find beginning of registration history section
-// initialize k:v pairs for each semester and the class data
-// once arrays are completed, print an html file based on index.html
